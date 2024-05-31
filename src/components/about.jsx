fe0be8f3e6b1ab6a22566db93971d45574c24221
Allison Ives <aives1@my.centennialcollege.ca>
2024-05-31 17:07:43 -0400

    netlify troubleshooting5

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -4,7 +4,7 @@ export default function About(){
         <>
         <div>
             <div style={{width: "50%", float: "left"}}>
-                <img src="\photosheadshot.jpg" width="400"></img>
+                <img src="photos\headshot.jpg" width="400"></img>
             </div>
             <div style={{width: "50%", float: "right"}}>
             <h2>About Me</h2>
